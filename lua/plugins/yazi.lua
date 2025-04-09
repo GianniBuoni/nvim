a6472705377540457dcc24c4e567bd199f7afe97
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-04-09 12:06:18 -0400

    update lock

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -2,24 +2,25 @@ return {
   {
     "mikavilpas/yazi.nvim",
     event = "VeryLazy",
+    -- key bindings
     keys = {
-      -- Keymappings
       {
-	"<leader>yy",
-	mode = { "n" },
-	"<cmd>Yazi<cr>",
-	desc = "Open [Y]azi at the current file",
+        "<leader>yy",
+        mode = { "n" },
+        "<cmd>Yazi<cr>",
+        desc = "Open [Y]azi at the current file",
       },
       {
-	"<leader>oo",
-	"<cmd>Yazi cwd<cr>",
-	desc = "[O]pen the file manager in nvim's working directory",
+        "<leader>oo",
+        "<cmd>Yazi cwd<cr>",
+        desc = "[O]pen the file manager in nvim's working directory",
       },
       {
-	"<c-up>",
-	"<cmd>Yazi toggle<cr>",
-	desc = "Resume the last yazi session",
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
       },
     },
+
   },
 }
